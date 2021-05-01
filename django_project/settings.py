60f30bba84c3be266bc37b4db1de5e0371324358
Robert Kenny <60153147+rlkenny98@users.noreply.github.com>
2021-05-01 14:03:24 -0700

    Set debug to True

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n1qxjrp7fbw0&d6ciwu8733)2alrn#c2x-z0!s3q@0dsyyl^wu'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['cpsc362app.herokuapp.com']
 
